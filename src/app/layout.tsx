879be5cf84fbeb97ec0fba7e40bf942731407eb9
la dj <ladj14013@gmail.com>
2025-09-13 06:58:30 +0000

    Medical Services  مكان خدمات طبية

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 
 export const metadata: Metadata = {
-  title: 'الخدمات الطبية',
+  title: 'Medical Services',
   description: 'ابحث واحجز موعدك التالي مع الطبيب.',
 };
 
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ export default function Logo() {
     <div className="flex items-center gap-2">
       <Stethoscope className="h-6 w-6 text-primary" />
       <h1 className="text-xl font-bold tracking-tighter text-foreground font-headline">
-        الخدمات الطبية
+        Medical Services
       </h1>
     </div>
   );
